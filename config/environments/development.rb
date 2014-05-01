79b47e3a5c2181e5db4d289608e1950e2fcbbeb7
oldtimer75 <dszeto@gmail.com>
2014-05-01 13:39:20 +0000

    updated some css and mail settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,8 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  ## Should change it back... to false...
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -41,7 +42,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
 
   # Adds additional error checking when serving assets at runtime.
